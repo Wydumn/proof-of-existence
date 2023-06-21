632740d4db7955cc658146f9ef3884ca40054d70
Wydumn <971703596@qq.com>
2023-06-21 22:17:26 +0800

    unsign transaction with signed payload

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -17,10 +17,11 @@ use sp_runtime::{
 		AccountIdLookup, BlakeTwo256, Block as BlockT, IdentifyAccount, NumberFor, One, Verify,
 	},
 	transaction_validity::{TransactionSource, TransactionValidity},
-	ApplyExtrinsicResult, MultiSignature,
+	ApplyExtrinsicResult, MultiSignature, SaturatedConversion
 };
 use sp_std::prelude::*;
 use pallet_insecure_randomness_collective_flip;
+use codec::Encode;
 
 #[cfg(feature = "std")]
 use sp_version::NativeVersion;
@@ -52,6 +53,8 @@ pub use sp_runtime::{Perbill, Permill};
 pub use pallet_template;
 
 pub use pallet_kitties;
+
+pub use pallet_ocw;
 
 /// An index to a block.
 pub type BlockNumber = u32;
@@ -273,6 +276,60 @@ impl pallet_template::Config for Runtime {
 	type RuntimeEvent = RuntimeEvent;
 }
 
+/// Configure the offchain runtime part
+impl<LocalCall> frame_system::offchain::CreateSignedTransaction<LocalCall> for Runtime
+where
+RuntimeCall: From<LocalCall>,
+{
+    fn create_transaction<C: frame_system::offchain::AppCrypto<Self::Public, Self::Signature>>(
+        call: RuntimeCall,
+        public: <Signature as sp_runtime::traits::Verify>::Signer,
+        account: AccountId,
+        nonce: Index,
+        ) -> Option<(RuntimeCall, <UncheckedExtrinsic as sp_runtime::traits::Extrinsic>::SignaturePayload)> {
+        let tip = 0;
+
+        let period =
+            BlockHashCount::get().checked_next_power_of_two().map(|c| c / 2).unwrap_or(2) as u64;
+        let current_block = System::block_number()
+            .saturated_into::<u64>()
+            .saturating_sub(1);
+        let era = generic::Era::mortal(period, current_block);
+        let extra = (
+            frame_system::CheckNonZeroSender::<Runtime>::new(),
+            frame_system::CheckSpecVersion::<Runtime>::new(),
+            frame_system::CheckTxVersion::<Runtime>::new(),
+            frame_system::CheckGenesis::<Runtime>::new(),
+            frame_system::CheckEra::<Runtime>::from(era),
+            frame_system::CheckNonce::<Runtime>::from(nonce),
+            frame_system::CheckWeight::<Runtime>::new(),
+            pallet_transaction_payment::ChargeTransactionPayment::<Runtime>::from(tip),
+            );
+        let raw_payload = SignedPayload::new(call, extra)
+            .map_err(|_| {
+                //log::warn!("Unable to create signed payload: {:?}", e);
+            })
+        .ok()?;
+        let signature = raw_payload.using_encoded(|payload| C::sign(payload, public))?;
+        let address = account;
+        let (call, extra, _) = raw_payload.deconstruct();
+        Some((call, (sp_runtime::MultiAddress::Id(address), signature.into(), extra)))
+    }
+}
+
+impl frame_system::offchain::SigningTypes for Runtime {
+    type Public = <Signature as sp_runtime::traits::Verify>::Signer;
+    type Signature = Signature;
+}
+
+impl<C> frame_system::offchain::SendTransactionTypes<C> for Runtime
+where
+RuntimeCall: From<C>,
+{
+    type Extrinsic = UncheckedExtrinsic;
+    type OverarchingCall = RuntimeCall;
+}
+
 impl pallet_insecure_randomness_collective_flip::Config for Runtime {}
 
 parameter_types! {
@@ -286,6 +343,11 @@ impl pallet_kitties::Config for Runtime {
 	type Currency = Balances;
 	type KittyPrice = KittyPrice;
 	type PalletId = KittyPalletId;
+}
+
+impl pallet_ocw::Config for Runtime {
+	type RuntimeEvent = RuntimeEvent;
+	type AuthorityId = pallet_ocw::crypto::TestAuthId;
 }
 
 // Create the runtime by composing the FRAME pallets that were previously configured.
@@ -306,6 +368,7 @@ construct_runtime!(
 		// Include the custom logic from the pallet-template in the runtime.
 		TemplateModule: pallet_template,
 		KittiesModule: pallet_kitties,
+		OcwModule: pallet_ocw,
 		RandomnessCollectiveFlip: pallet_insecure_randomness_collective_flip,
 	}
 );
